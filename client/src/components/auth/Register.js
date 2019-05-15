93192562556d7ff5e4addc7f17247dacdc1b097c
natarajan1993 <natraj_93@yahoo.com>
2019-05-14 20:16:58 -0500

    Added login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,7 @@
 import React, {Fragment, useState} from 'react'
+import {Link} from 'react-router-dom'
+
+// import axios from 'axios'
 
 const Register = () => {
 
@@ -13,12 +16,26 @@ const Register = () => {
     // ... is the spread operator used to copy objects
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
             console.log('Passwords do not match');
         }else{
-            console.log(formData);
+            // Send a register request using axios
+            // const newUser = {name, email, password}
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'Content-type':'application/json'
+            //         }
+            //     };
+            //     const body = JSON.stringify(newUser);
+            //     const res = await axios.post('/api/users',body,config);
+            //     console.log(res.data);
+            // } catch (err) {
+            //     console.error(err.response.data);
+            // }
+            console.log('Successfully Created')
         }
     };
 
@@ -62,7 +79,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   )
