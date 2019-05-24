0421d3b2afaf75fbc6fad86f32c4a9de2c8983ae
natarajan1993 <natraj_93@yahoo.com>
2019-05-23 21:49:52 -0500

    Added redux custom PrivateRoute component to check if user is authenticated before accessing private routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {Provider} from 'react-redux';
 import store from './store'
 import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 import {loadUser} from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
 
@@ -35,6 +37,7 @@ const App = () =>{
           <Switch>
             <Route exact path ='/register' component={Register}></Route>
             <Route exact path ='/login' component={Login}></Route>
+            <PrivateRoute exact path ='/dashboard' component={Dashboard}></PrivateRoute>
           </Switch>
         </section>
       </Fragment>
