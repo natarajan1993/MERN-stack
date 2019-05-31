2b475f28372baacf2b3c02e738d1f55af7c3a0da
natarajan1993 <natraj_93@yahoo.com>
2019-05-30 19:35:19 -0500

    Added get github repos, get all profiles and profile by ID actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {setAlert} from './alert'
-import {GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE} from './types';
+import {GET_PROFILE,GET_PROFILES, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE, GET_REPOS} from './types';
 
 // Action to Get current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -9,6 +9,63 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText,
+                      status: err.response.status}
+        });
+    }
+}
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({type:CLEAR_PROFILE}); // Prevents any residue of previous users that have logged in
+    try {
+        const res = await axios.get('/api/profile')
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText,
+                      status: err.response.status}
+        });
+    }
+}
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+    dispatch({type:CLEAR_PROFILE}); // Prevents any residue of previous users that have logged in
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`)
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText,
+                      status: err.response.status}
+        });
+    }
+}
+
+// Get github repos
+export const getGithubRepo = username => async dispatch => {
+    dispatch({type:CLEAR_PROFILE}); // Prevents any residue of previous users that have logged in
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`)
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data
         });
     } catch (err) {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_PROFILES, GET_REPOS } from "../actions/types";
 
 const initialState = {
     profile: null, // All the users profile data
@@ -18,6 +18,12 @@ export default function(state=initialState,action){
                 profile:payload,
                 loading: false
             };
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
+                loading: false
+            }
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -31,6 +37,12 @@ export default function(state=initialState,action){
                 repos: [],
                 loading: false
             }
+        case GET_REPOS:
+            return({
+                ...state,
+                repos: payload,
+                loading: false
+            })
         default:
             return state;
     }
